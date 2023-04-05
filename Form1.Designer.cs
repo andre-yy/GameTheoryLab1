9a8d0758b63711af209ef638c2d4978f58bcf670
andre-yy <andrey19122000@gmail.com>
2023-04-05 22:36:08 +0300

    Реализована возможность ввода элементов матрицы в зависимости от выбранных размеров

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,181 @@ namespace GameTheoryLab1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tabControl1.Location = new System.Drawing.Point(675, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(307, 527);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(299, 498);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(299, 461);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Исходная матрица";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(166, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "X:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(136, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Y:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Изменить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 492);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Загрузить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(312, 492);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Сохранить";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 492);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Имя файла";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(101, 492);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(982, 527);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,112 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace GameTheoryLab1
 {
     public partial class Form1 : Form
     {
+        int X = 2, Y = 2;
+        
+        //public static List<TextBox> TextBoxes = new List<TextBox>();
+        private const string _TABLE_PANEL_NAME = "tableLayoutPanel";
         public Form1()
         {
             InitializeComponent();
+            float[,] A = new float[X, Y];
+            ShowMatrix(X, Y);
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void ShowMatrix(int rowCount, int columnCount)
+        {
+            //создание TableLayoutPanel
+            CreateTablePanel(rowCount, columnCount);
+
+            //находим по имени только что созданную TableLayoutPanel
+            var tablePanel = this.Controls
+                                    .Find(_TABLE_PANEL_NAME, true)
+                                    .First() as TableLayoutPanel;
+
+            //вставляем текстбоксы
+            for (int i = 0; i < columnCount; i++)
+            {
+                for (int j = 0; j < rowCount; j++)
+                {
+                    var txt = $"textBox{i}{j}";
+                    //новый текстбокс с текстовкой, именем, шириной
+                    var tb = new TextBox { Text = "", Name = txt, Width = 30 };
+                    //добавляем
+                    tablePanel.Controls.Add(tb);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            X = Convert.ToInt32(textBox1.Text);
+            Y = Convert.ToInt32(textBox2.Text);
+            ShowMatrix(X, Y);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            FileStream file = new FileStream(Convert.ToString(textBox3), FileMode.Open);
+            
+        }
+
+        private void CreateTablePanel(int rowCount, int columnCount)
+        {
+            //если были ранее созданы с тем же именем TableLayoutPanel
+            //то удаляем их
+            var oldPanels = this.Controls.Find(_TABLE_PANEL_NAME, true);
+            Array.ForEach(oldPanels, new Action<Control>(c => this.Controls.Remove(c)));
+
+            //позиция для новой панели
+            Point pos = new Point { X = 10, Y = 65 };
+           
+            TableLayoutPanel tablePanel = new TableLayoutPanel
+            {
+
+                Location = pos,
+                Name = _TABLE_PANEL_NAME,
+                RowCount = rowCount,
+                ColumnCount = columnCount,
+                AutoScroll = true,
+                Width = 500,
+                Height = 300
+                //AutoSize = true,
+                //MaximumSize = size
+                
+            };
+
+            //назначаем размеры для строк и столбцов
+            for (int i = 0; i < tablePanel.RowStyles.Count; i++)
+            {
+                tablePanel.RowStyles[i].SizeType = SizeType.Absolute;
+                tablePanel.RowStyles[i].Height = 30;
+            }
+            for (int i = 0; i < tablePanel.ColumnStyles.Count; i++)
+            {
+                tablePanel.ColumnStyles[i].SizeType = SizeType.Absolute;
+                tablePanel.ColumnStyles[i].Width = 40;
+            }
+
+            //добавляем панель
+            this.Controls.Add(tablePanel);
+        }
+
     }
 }
